ba22aa78c6b998630c484e15454018c1cf179323
allanjluke <allanjluke@gmail.com>
2015-09-28 19:33:03 -0400

    junk n things

---
+++ dragulawork/dummy.js
@@ -0,0 +1,10 @@
+/**
+ * Created by allan on 2015-09-28.
+ */
+$(document).ready(function(){
+
+
+
+    dragula([document.querySelector('#line1'), document.querySelector('#line2')]);
+    dragula([document.getElementsByClassName("chords"),document.getElementsByClassName("chords")]);
+});
