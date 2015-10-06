835d847baa96335e2e882157eeff094bc4f67001
Abhinav Thukral <abhinav@abhinavthukral.com>
2015-10-06 17:20:28 -0400

    Submission Ready

--- server.js
+++ server.js
@@ -150,7 +150,6 @@ http.createServer(function(request, response) {
         });
     }
 
-//}).listen(process.env.PORT, process.env.IP);
 }).listen("3000", "127.0.0.1");
 
-//console.log('Server Running at http://127.0.0.1:3000  CNTL-C to quit');
+console.log('Server Running at http://127.0.0.1:3000  CNTL-C to quit');
